150b0e16aa18d091b8a4773a534a34eae0645284
Vitor moretti <vitorfrancamoretti@gmail.com>
2026-05-18 16:35:51 -0300

    Create atividade-07.cpp

---
+++ 2-bimestre/KevinSilva-VitorMoretti/Lang-C/Lista-02_cpp/atividade-07.cpp
@@ -0,0 +1,28 @@
+//Ler 5 numeros e verificar a ordem
+#include <iostream>
+
+std::string verificarOrdem(int n1, int n2, int n3, int n4, int n5) {
+
+    if (n1 < n2 && n2 < n3 && n3 < n4 && n4 < n5) {
+        return "Ordem crescente";
+    }
+    else if (n1 > n2 && n2 > n3 && n3 > n4 && n4 > n5) {
+        return "Ordem decrescente";
+    }
+    else {
+        return "Nao ha ordem";
+    }
+}
+
+int main() {
+
+    int n1, n2, n3, n4, n5;
+
+    std::cout << "Digite 5 numeros:\n";
+
+    std::cin >> n1 >> n2 >> n3 >> n4 >> n5;
+
+    std::cout << verificarOrdem(n1, n2, n3, n4, n5);
+
+    return 0;
+}             
