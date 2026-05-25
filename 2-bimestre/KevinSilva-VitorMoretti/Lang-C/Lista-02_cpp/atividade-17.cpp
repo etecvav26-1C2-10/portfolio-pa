e0b101824771fb474872adf18028d2c1bf241cc9
Vitor moretti <vitorfrancamoretti@gmail.com>
2026-05-25 14:47:39 -0300

    Implement sum of positive numbers from two vectors
    Added functionality to calculate and display the sum of positive numbers from both vectors.

--- 2-bimestre/KevinSilva-VitorMoretti/Lang-C/Lista-02_cpp/atividade-17.cpp
+++ 2-bimestre/KevinSilva-VitorMoretti/Lang-C/Lista-02_cpp/atividade-17.cpp
@@ -1,4 +1,4 @@
-//  Ler 10 números em um vetor.
+// Ler 10 números em um vetor.
 // Criar outro vetor com os sinais invertidos.
 // Exibir a soma dos valores positivos dos dois vetores.
 
@@ -6,7 +6,7 @@
 
 int main() {
     
-    int num_p[10], i, num_s[10]; //num_p = Primeiro vetor num_s = 2º
+    int num_p[10], i, num_s[10], soma; //num_p = Primeiro vetor num_s = 2º
     
     for (i=0; i < 10; i++){ //Pede 10 numeros
         std::cout << "Digite um numero: ";
@@ -17,12 +17,27 @@ int main() {
         if(num_p[i] > 0){
             num_s[i] = num_p[i] * -1; //para negativo
         }
-        else if (num_p[i] < 0) {
-            num_s[i] = num_p[i]; //para positivo ??
-        }
-        
-        std::cout << "\n" << num_s[i]; //teste
     }
+    
+    for(i=0; i < 10; i++){
+        if(num_p[i] < 0){
+            num_s[i] = num_p[i] * -1; //para positivo
+        }
+        std::cout << "\n" << num_s[i]; // Exibe Arr 2
+    }
+    
+    //Soma dos positivos
+    for(i = 0; i < 10; i++) {
+
+        if(num_p[i] > 0) {
+            soma += num_p[i];
+        }
+
+        if(num_s[i] > 0) {
+            soma += num_s[i];
+        }
+    }
+    std::cout << "\n\nSoma dos positivos: " << soma;
     
     return 0;
 }
